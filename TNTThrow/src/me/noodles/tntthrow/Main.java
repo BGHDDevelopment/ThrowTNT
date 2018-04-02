fcefb64017310ae5f517ffd6b0b233acffe07e22
stephan <stephan@172.28.66.37>
2018-04-02 16:45:20 -0700

    Created Project

---
+++ TNTThrow/src/me/noodles/tntthrow/Main.java
@@ -0,0 +1,21 @@
+package me.noodles.tntthrow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	
+	
+	@Override
+	public void onEnable() {	
+	getServer().getPluginManager().registerEvents(new Command(), this);
+	getServer().getConsoleSender().sendMessage("ThrowTNT has been enabled!");
+	}
+	
+	@Override
+	public void onDisable() {
+	getServer().getConsoleSender().sendMessage("ThrowTNT has been disabled!");
+	}
+	
+	
+}
