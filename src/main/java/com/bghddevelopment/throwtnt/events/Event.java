50892958a11e487b9f1170b4bd59a4edb82f63b3
BGHDDevelopment <ceo@bghddevelopment.com>
2022-12-29 09:31:40 -0800

    cleaned up code Update checker

---
+++ src/main/java/com/bghddevelopment/throwtnt/events/Event.java
@@ -0,0 +1,31 @@
+package com.bghddevelopment.throwtnt.events;
+
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class Event implements Listener {
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+        ItemStack item = player.getInventory().getItemInHand();
+        if ((player.hasPermission("throwtnt.use")) && ((event.getAction() == Action.LEFT_CLICK_AIR) || (event.getAction() == Action.LEFT_CLICK_BLOCK)) && (item.getType() == Material.TNT)) {
+            event.setCancelled(true);
+            if (item.getAmount() == 1) {
+                player.getInventory().remove(item);
+            } else {
+                item.setAmount(item.getAmount() - 1);
+            }
+            TNTPrimed tnt = world.spawn(player.getLocation(), TNTPrimed.class);
+            tnt.setVelocity(event.getPlayer().getLocation().getDirection().multiply(1.5D));
+        }
+    }
+}
---
+++ src/main/java/com/bghddevelopment/throwtnt/util/Color.java
@@ -0,0 +1,16 @@
+package com.bghddevelopment.throwtnt.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Color {
+
+    public static String translate(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+}
